fbc84592fbb7d57cd1111e73c05cda1ca48df61b
Matti J. Karki <mjk@iki.fi>
2023-05-27 09:13:51 +0300

    Default binary under Windows does not support compression

--- make.bat
+++ make.bat
@@ -1,2 +1,3 @@
 @echo off
-sbcl --load pricewatch.lisp --eval "(sb-ext:save-lisp-and-die """"pricewatch.exe"""" :executable t :compression 9 :application-type :console :toplevel 'main)"
+rem sbcl --load pricewatch.lisp --eval "(sb-ext:save-lisp-and-die """"pricewatch.exe"""" :executable t :compression 22 :application-type :console :toplevel 'main)"
+sbcl --load pricewatch.lisp --eval "(sb-ext:save-lisp-and-die """"pricewatch.exe"""" :executable t :application-type :console :toplevel 'main)"
